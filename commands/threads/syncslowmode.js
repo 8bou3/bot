ece9affa5c0d9a2b847b84f311910321a85d7758
Spirit <i8bou3@gmail.com>
2021-10-09 01:42:29 +0300

    Fix syncslowmode command

--- commands/threads/syncslowmode.js
+++ commands/threads/syncslowmode.js
@@ -1,35 +1,28 @@
-const i18n = require("i18n")
+const i18n = require('i18n')
 
 module.exports = {
-  name: "syncslowmode",
-  usage: "<boolean: toggle>",
-  permissions: ["MANAGE_THREADS"],
-  runPermissions: ["EMBED_LINKS", "SEND_MESSAGES", "MANAGE_THREADS"],
-  description: i18n.__("syncslowmode.description"),
+  name: 'syncslowmode',
+  usage: '<boolean: toggle>',
+  permissions: ['MANAGE_THREADS'],
+  runPermissions: ['EMBED_LINKS', 'SEND_MESSAGES', 'MANAGE_THREADS'],
+  description: i18n.__('syncslowmode.description'),
   options: [
     {
       type: 5,
-      name: "boolean",
-      description: "True: on, false: off",
-    },
+      name: 'boolean',
+      description: 'True: on, false: off'
+    }
   ],
-  async execute(interaction, Data) {
-    let toggle = interaction.options.getBoolean("boolean")
-      ? interaction.options.getBoolean("boolean")
+  async execute (interaction, Data) {
+    let toggle = interaction.options.getBoolean('boolean')
+      ? interaction.options.getBoolean('boolean')
       : Data.channel.threads.syncSlowmode
       ? false
-      : true;
-    
-    if (toggle) {
-      Data.channel.threads.syncSlowmode = undefined
-      await Data.channel.save()
-      
-      interaction.editReply("Sync slowmode off")
-    } else {
-      Data.channel.threads.syncSlowmode = true
-      await Data.channel.save()
-      
-      interaction.editReply("Sync slowmode on")
-    }
-  },
-};
+      : true
+
+    Data.channel.threads.syncSlowmode = toggle
+    await Data.channel.save()
+
+    interaction.editReply(`Sync slowmode \`${toggle}\``)
+  }
+}
