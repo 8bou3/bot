e250756efa65695da90972c89a1758ce50cead98
Spirit <i8bou3@gmail.com>
2021-10-15 00:47:22 +0300

    Create create_ticket selectMenu

---
+++ selectMenus/tickets/create_ticket.js
@@ -0,0 +1,127 @@
+const { Collection } = require("discord.js");
+const i18n = require("i18n");
+const channelModel = require("../../models/channel");
+
+module.exports = {
+  customId: "create_ticket",
+  defer: true,
+  async select(interaction, Data) {
+    if (!Data.guild.tickets?.mode)
+      return interaction.editReply(
+        "**Error:**\n> Corruption in database `selectMenus(create_ticket)`\n\n||Report this to the support: https://discord.gg/YywkMTmHHb||"
+      );
+    else if (
+      Data.guild.tickets?.mode === "threads" &&
+      ["NONE", "TIER_1"].includes(interaction.guild.premiumTier)
+    )
+      return interaction.editReply(
+        "**Error:**\n> Tickets with threads only work in servers with tier2 & above `selectMenus(create_ticket)`\n\n||Report this to the support: https://discord.gg/YywkMTmHHb||"
+      );
+
+    const channels = await interaction.guild.channels.fetch();
+    const messagesChannel = channels.get(Data.guild.tickets.messages.channel);
+    const openTicketMessage = await messagesChannel?.messages.fetch(
+      Data.guild.tickets.messages.openTicket
+    );
+    const channel = channels.get(Data.guild.tickets.channel);
+    if (!channel)
+      return interaction.editReply(
+        "**Error:**\n> No tickets channel `selectMenus(create_ticket)`\n\n||Report this to the support: https://discord.gg/YywkMTmHHb||"
+      );
+    const parent = channels.get(
+      Data.guild.tickets.parent
+    );
+    if (!parent)
+      return interaction.editReply(
+        "**Error:**\n> No parent channel `selectMenus(create_ticket)`\n\n||Report this to the support: https://discord.gg/YywkMTmHHb||"
+      );
+
+    const cooldownAmount = Data.guild.tickets.cooldown * 1000;
+    if (!interaction.client.cooldowns.has("create_ticket"))
+      interaction.client.cooldowns.set("create_ticket", new Collection());
+    const timestamps = interaction.client.cooldowns.get("create_ticket");
+    if (timestamps.has(interaction.user.id)) {
+      const expirationTime =
+        timestamps.get(interaction.user.id) + cooldownAmount;
+      if (Date.now() < expirationTime) {
+        const timeLeft = (expirationTime - Date.now()) / 1000;
+        return interaction.editReply({
+          embeds: [
+            {
+              color: `${Data.color}`,
+              description: i18n.__mf("embeds.tickets.cooldown.description", {
+                timeLeft: timeLeft.toFixed(1),
+              }),
+              title: i18n.__("embeds.tickets.cooldown.title"),
+            },
+          ],
+          ephemeral: true,
+        });
+      } else timestamps.delete(interaction.user.id);
+    }
+
+    timestamps.set(interaction.user.id, Date.now());
+    setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+    let ticket;
+
+    switch (Data.guild.tickets?.mode) {
+      case "threads":
+        const startMessage = await channel.send(
+          openTicketMessage
+            ? {
+                content: openTicketMessage.content,
+                attachments: openTicketMessage.attachments,
+                embeds: openTicketMessage.embeds,
+              }
+            : `New Ticket!! <@&${Data.guild.roles.supportTeam}>`
+        );
+        ticket = await parent.threads.create({
+          name: `open-${Data.guild.tickets.counter}`,
+          startMessage: startMessage,
+          autoArchiveDuration: 60, //60 | 1440 | 4320 | 10080
+          type: 12,
+        });
+
+        break;
+
+      case "classic":
+        ticket = await parent.clone({
+          name: `open-${Data.guild.tickets.counter}`,
+          type: "GUILD_TEXT",
+          parent: parent,
+        });
+        await ticket.permissionOverwrites.create(
+          interaction.user,
+          { VIEW_CHANNEL: true, SEND_MESSAGES: true },
+          { type: 1, reason: "Ticket member" }
+        );
+        channel.send(
+          openTicketMessage
+            ? {
+                content: openTicketMessage.content,
+                attachments: openTicketMessage.attachments,
+                embeds: openTicketMessage.embeds,
+              }
+            : `New Ticket!! <@&${Data.guild.roles.supportTeam}>`
+        );
+
+        break;
+    }
+
+    Data.ticket = new channelModel({
+      ticket: {
+        status: "open",
+        number: Data.guild.tickets.counter,
+        members: [interaction.user.id],
+      },
+      channelId: ticket.id,
+      guildId: interaction.guildId,
+    });
+    Data.guild.tickets.counter += 1;
+    Data.ticket.save();
+    Data.guild.save();
+
+    interaction.editReply(`You selected ${interaction.values.join(", ")}!`);
+  },
+};
