ea1b08c9f4c4bab1408cb1c3eb70d5421304277f
Spirit <i8bou3@gmail.com>
2021-10-23 21:19:07 +0300

    Fix leave join guild log

--- events/guilds/guildCreate.js
+++ events/guilds/guildCreate.js
@@ -1,18 +1,12 @@
 module.exports = {
   name: "guildCreate",
   async execute(guild, client) {
-    client.shard.broadcastEval((client) =>
-      client.channels.cache.get("894601362808123455")?.send(
-        `**Joined ${guild.name}:**\n> Member Count: ${
-          guild.memberCount
-        }\n> Total Servers: ${client.shard
-          .fetchClientValues("guilds.cache.size")
-          .then((results) => {
-            return results.reduce((acc, guildCount) => acc + guildCount, 0);
-          })}\n> Date: <t:${Math.round(
-          Date.now() / 100
-        )}:D>\n> Time: <t:${Math.round(Date.now() / 100)}:T>`
-      )
+    client.channels.cache.get("894601362808123455")?.send(
+      `**Joined ${guild.name}:**\n> Member Count: ${
+        guild.memberCount
+      }\n> Total Servers: ${client.guilds.cache.size}\n> Date: <t:${Math.round(
+        Date.now() / 100
+      )}:D>\n> Time: <t:${Math.round(Date.now() / 100)}:T>`
     );
   },
 };
