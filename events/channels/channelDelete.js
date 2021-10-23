6fbb71b9d913f708ad79584720ecba8dc35bd4ef
Spirit <i8bou3@gmail.com>
2021-10-23 19:36:56 +0300

    Fix all the things

--- events/channels/channelDelete.js
+++ events/channels/channelDelete.js
@@ -1,11 +1,34 @@
-const i18n = require("i18n");
-
 const guildModel = require("../../models/guild");
-const channelModel = require("../../models/channel");
+const ticketModel = require("../../models/ticket");
+const { deleteElements } = require("../../functions/deleteElements");
 
 module.exports = {
   name: "channelDelete",
-  async execute(channel, client) {
-    
-  }
-}
+  async execute(channel) {
+    let Data = new Object();
+
+    Data.ticket = await ticketModel.findById(channel.id);
+    if (Data.ticket) Data.ticket.remove();
+
+    Data.guild = await guildModel.findById(channel.guild.id);
+
+    if (Data.guild) {
+      if (Data.guild.channels.suggestions === channel.id)
+        Data.guild.channels.suggestions = undefined;
+      if (Data.guild.messages?.channel === channel.id)
+        Data.guild.messages = undefined;
+      if (Data.guild.tickets.parent === channel.id) {
+        Data.guild.tickets.parent = undefined;
+        Data.guild.tickets.mode = undefined;
+      }
+      if (Data.guild.tickets.channel === channel.id) {
+        Data.guild.tickets.channel = undefined;
+        Data.guild.tickets.mode = undefined;
+      }
+      deleteElements(Data.guild.channels.autoCrosspost, channel.id);
+      deleteElements(Data.guild.channels.syncSlowmode, channel.id);
+      deleteElements(Data.guild.channels.blacklist, channel.id);
+      Data.guild.save();
+    }
+  },
+};
