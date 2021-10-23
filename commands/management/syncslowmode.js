a8187d3e40d4d1a4cbfb452cabce3d48049983f7
Spirit <i8bou3@gmail.com>
2021-10-23 19:34:37 +0300

    Fix all the things

--- commands/management/syncslowmode.js
+++ commands/management/syncslowmode.js
@@ -1,26 +1,38 @@
-const i18n = require('i18n')
+const i18n = require("i18n");
+const { deleteElements } = require("../../functions/deleteElements");
 
 module.exports = {
-  name: 'syncslowmode',
-  usage: '<boolean: toggle>',
-  permissions: ['MANAGE_THREADS'],
-  runPermissions: ['EMBED_LINKS', 'SEND_MESSAGES', 'MANAGE_THREADS'],
-  description: i18n.__('syncslowmode.description'),
+  name: "syncslowmode",
+  usage: "<boolean: toggle>",
+  permissions: ["MANAGE_THREADS"],
+  runPermissions: ["EMBED_LINKS", "SEND_MESSAGES", "MANAGE_THREADS"],
+  description: i18n.__("syncslowmode.description"),
   options: [
     {
       type: 5,
-      name: 'boolean',
-      description: 'True: on, false: off'
-    }
+      name: "boolean",
+      description: "True: on, False: off",
+    },
   ],
-  async execute (interaction, Data) {
-    let toggle = interaction.options.getBoolean('boolean')
+  async execute(interaction, Data) {
+    await interaction.deferReply();
+    let toggle = interaction.options.getBoolean("boolean");
     if (toggle === null)
-      toggle = Data.channel.threads.syncSlowmode ? false : true
+      toggle = Data.guild.channels.syncSlowmode.includes(interaction.channel.id)
+        ? false
+        : true;
 
-    Data.channel.threads.syncSlowmode = toggle
-    await Data.channel.save()
+    if (
+      toggle &&
+      !Data.guild.channels.syncSlowmode.includes(interaction.channel.id)
+    ) {
+      Data.guild.channels.syncSlowmode.push(interaction.channel.id);
+      await Data.guild.save();
+    } else if (!toggle) {
+      deleteElements(Data.guild.channels.syncSlowmode, interaction.channel.id);
+      await Data.guild.save();
+    }
 
-    interaction.editReply(`Sync slowmode \`${toggle}\``)
-  }
-}
+    interaction.editReply(`Sync slowmode \`${toggle}\``);
+  },
+};
