af469e27061119056472b5b2c578cd13766fcfbc
Spirit <i8bou3@gmail.com>
2021-10-23 19:39:08 +0300

    Fix all the things

---
+++ functions/loadEvents.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+function loadEvents(client) {
+  const eventFolders = fs.readdirSync("./events");
+  for (const folder of eventFolders) {
+    const eventFiles = fs
+      .readdirSync(`./events/${folder}`)
+      .filter((file) => file.split(".").pop() === "js");
+    for (const file of eventFiles) {
+      if (eventFiles.length <= 0) {
+        console.log("client couldn't find files in events folder.");
+      } else {
+        const event = require(`../events/${folder}/${file}`);
+        if (event.once) {
+          client.once(event.name, (...args) => event.execute(...args, client));
+        } else {
+          client.on(event.name, (...args) => event.execute(...args, client));
+        }
+      }
+    }
+  }
+  console.log("Events loaded");
+}
+
+module.exports = {
+  loadEvents,
+};
