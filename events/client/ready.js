e732d254b25c14332f0838e188b2abb41b32575f
8bou3 <i8bou3@gmail.com>
2022-01-19 23:33:23 +0300

    Remove unneeded logs

--- events/client/ready.js
+++ events/client/ready.js
@@ -9,22 +9,6 @@ module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
-    client.cache.console = client.channels.cache.get("904100576894283806");
-    client.cache.errors = client.channels.cache.get("904375875565420544");
-
-    if (client.cache.console) {
-      console.log = (d) => client.cache.console.send(d.toString());
-    }
-    if (client.cache.errors) {
-      console.error = (d) => client.cache.console.send(d.toString());
-    }
-
-    console.log("==========================================");
-    console.log(`client.cache.console: ${client.cache.console}`);
-    console.log(`client.cache.errors: ${client.cache.errors}`);
-    console.log(`client.cache.guilds: ${client.cache.guilds.size}`);
-    console.log(`client.cache.cooldowns: ${client.cache.cooldowns.size}`);
-
     mongooseConnect(config.mongoPath);
     loadCommands(client);
     loadButtons(client);
